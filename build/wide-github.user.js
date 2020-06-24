6e8222059e96fd6eb000375d70283f619ca71b2a
Nexarius <github@nexarius.xyz>
2020-06-24 21:50:44 +0200

    clean it up

--- build/wide-github.user.js
+++ build/wide-github.user.js
@@ -11,8 +11,9 @@
 // @contributor Paul "Joey" Clark (https://github.com/joeytwiddle)
 // @contributor Robert Laverty (https://github.com/roblav96)
 // @contributor Amir Meimari (https://github.com/amirmeimari)
+// @contributor Nex4rius (https://github.com/nex4rius)
 // @license     MIT; https://raw.githubusercontent.com/xthexder/wide-github/master/LICENSE
-// @version     1.4.2
+// @version     1.4.3
 // @icon        https://raw.githubusercontent.com/xthexder/wide-github/master/icon.png
 // @homepageURL https://github.com/xthexder/wide-github
 // @supportURL  https://github.com/xthexder/wide-github/issues
@@ -20,68 +21,68 @@
 // @grant       none
 // ==/UserScript==
 
-var styleSheet = "" +
-"body:not(.wgh-disabled) .application-main .container {" +
-  "width: auto !important;" +
-  "padding-left: 16px !important;" +
-  "padding-right: 16px !important;" +
-  "margin-left: 0px !important;" +
-  "min-width: 980px;" +
-"}" +
-"body:not(.wgh-disabled) .application-main .container-lg," +
-"body:not(.wgh-disabled) .footer.container-lg {" +
-  "max-width: none !important;" +
-  "margin-left: 0px !important;" +
-"}" +
+var styleSheet = `
+body:not(.wgh-disabled) .application-main .container {
+  width: auto !important;
+  padding-left: 16px !important;
+  padding-right: 16px !important;
+  margin-left: 0px !important;
+  min-width: 980px;
+}
+body:not(.wgh-disabled) .application-main .container-lg,
+body:not(.wgh-disabled) .footer.container-lg {
+  max-width: none !important;
+  margin-left: 0px !important;
+}
 
 // New github design
-"body:not(.wgh-disabled) .application-main .container-xl {" +
-  "max-width: none !important;" +
-"}" +
+body:not(.wgh-disabled) .application-main .container-xl {
+  max-width: none !important;
+}
 
 // Floating PR toolbar
-"body:not(.wgh-disabled) .pr-toolbar {" +
-  "margin-left: -16px !important;" +
-  "margin-right: -16px !important;" +
-  "padding-left: 16px !important;" +
-  "padding-right: 16px !important;" +
-"}" +
+body:not(.wgh-disabled) .pr-toolbar {
+  margin-left: -16px !important;
+  margin-right: -16px !important;
+  padding-left: 16px !important;
+  padding-right: 16px !important;
+}
 
 // Repository Issues
-"body:not(.wgh-disabled) #js-repo-pjax-container .repository-content .discussion-timeline {" +  // Issue body
-  "width: 100% !important;" +
-"}" +
-"body:not(.wgh-disabled) .repository-content .timeline-new-comment {" + // New Issue / issue comment form
-  "max-width: 100% !important;" +
-"}" +
-"body:not(.wgh-disabled) .repository-content .inline-comments .comment-holder," + // Diff / code comments
-"body:not(.wgh-disabled) .repository-content .inline-comments .inline-comment-form-container," +
-"body:not(.wgh-disabled) .repository-content .inline-comments .inline-comment-form," +
-"body:not(.wgh-disabled) .repository-content #all_commit_comments .commit-comments-heading," +
-"body:not(.wgh-disabled) .repository-content #all_commit_comments .comment-holder {" +
-  "max-width: inherit !important;" +
-"}" +
+body:not(.wgh-disabled) #js-repo-pjax-container .repository-content .discussion-timeline {  // Issue body
+  width: 100% !important;
+}
+body:not(.wgh-disabled) .repository-content .timeline-new-comment { // New Issue / issue comment form
+  max-width: 100% !important;
+}
+body:not(.wgh-disabled) .repository-content .inline-comments .comment-holder, // Diff / code comments
+body:not(.wgh-disabled) .repository-content .inline-comments .inline-comment-form-container,
+body:not(.wgh-disabled) .repository-content .inline-comments .inline-comment-form,
+body:not(.wgh-disabled) .repository-content #all_commit_comments .commit-comments-heading,
+body:not(.wgh-disabled) .repository-content #all_commit_comments .comment-holder {
+  max-width: inherit !important;
+}
 
 // Repository graph page
-"body:not(.wgh-disabled) .repository-content .capped-card-content {" + // Graph cards on contributors / graph list
-  "width: 100% !important;" +
-"}" +
+body:not(.wgh-disabled) .repository-content .capped-card-content { // Graph cards on contributors / graph list
+  width: 100% !important;
+}
 
 // Gists
-"body:not(.wgh-disabled) .gist-content-wrapper .container {" +
-  "width: auto !important;" +
-  "margin-left: 20px !important;" +
-  "margin-right: 20px !important;" +
-  "min-width: 980px;" +
-"}" +
-"body:not(.wgh-disabled) .gist-content-wrapper .container-lg {" +
-  "max-width: none !important;" +
-"}" +
-"body:not(.wgh-disabled) .gist-content-wrapper .container-lg .h-card {" +
-  "width: 253px !important;" +
-"}" +
+body:not(.wgh-disabled) .gist-content-wrapper .container {
+  width: auto !important;
+  margin-left: 20px !important;
+  margin-right: 20px !important;
+  min-width: 980px;
+}
+body:not(.wgh-disabled) .gist-content-wrapper .container-lg {
+  max-width: none !important;
+}
+body:not(.wgh-disabled) .gist-content-wrapper .container-lg .h-card {
+  width: 253px !important;
+}
 
-"";
+`;
 
 (function () {
   var s = document.createElement('style');
